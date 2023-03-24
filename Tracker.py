fb90a16167016e64ee158167850d02c226501815
suryateja-challa <challasuryateja29@gmail.com>
2023-03-24 14:46:31 +0530

    Tracker for Face landmarks, left & right hand landamrks and pose detection.

--- Tracker.py
+++ Tracker.py
@@ -13,7 +13,6 @@ with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=
 
         image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         results = holistic.process(image)
-        #print(results.face_landmarks)
 
         image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
